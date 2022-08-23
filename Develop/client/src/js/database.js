4b1880b2cfa2ec2ce7eb3fabb3b1d0801b6f2bc9
Jeffrey <wzhang@wzhangs-MacBook-Pro.local>
2022-08-22 21:21:25 -0700

    added putDb and getDb logic

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,11 +13,27 @@ const initdb = async () =>
   });
 
 export const putDb = async (content) => {
-
+  // Creates connection to the database, specifies what version we will use.
+  const jateDb = await openDB('jate', 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+  const request = store.put({ data: content });
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
 };
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
 
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get(1);
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
 };
 
 
