8e1efb9a676beb7476ed044d4a7b807d19c00db9
Jeffrey <wzhang@wzhangs-MacBook-Pro.local>
2022-08-22 21:20:46 -0700

    added logic for installing PWA

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  // Creates connection to the database, specifies what version we will use.
+  const jateDb = await openDB('jate', 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+  const request = store.put({ data: content });
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get(1);
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
+
 
 initdb();
